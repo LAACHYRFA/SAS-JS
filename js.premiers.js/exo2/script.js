508a5243a17944c422bf4fc4bb74a6c62a385213
LAACHYRFA <laachyr.farah.solicode@gmail.com>
2025-09-19 13:39:41 +0100

    Delete js.premiers.js/exo2 directory

--- js.premiers.js/exo2/script.js
+++
@@ -1,8 +0,0 @@
-let note =prompt("entrez votre note")
-if (note<10) {
-    alert("Insuffisant");
-    } else if(note >= 10 && note <= 12) {
-    alert("Passable");
-    } else {
-        alert("bien");
-    }
