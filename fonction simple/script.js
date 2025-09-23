670dc8c5005814ceb0f61dd1eec2d6abcaadda5f
LAACHYRFA <laachyr.farah.solicode@gmail.com>
2025-09-23 09:53:14 +0100

    Add files via upload

---
+++ fonction simple/script.js
@@ -0,0 +1,11 @@
+// Définition d'une fonction qui additionne deux nombres
+function addition(a, b) {
+    return a + b;
+  }
+  
+  // Appel de la fonction avec 8 et 5
+  let somme = addition(8, 5);
+  
+  // Affichage du résultat dans la console
+  console.log("La somme est : " + somme);
+  
