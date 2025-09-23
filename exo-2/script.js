cde9c304eff11f3b29228a50f3fa153a707cf4b2
LAACHYRFA <laachyr.farah.solicode@gmail.com>
2025-09-23 09:45:52 +0100

    Add files via upload

---
+++ exo-2/script.js
@@ -0,0 +1,9 @@
+let note =prompt("entrez votre note")
+if (note<10) {
+    alert("Insuffisant");
+    } else
+     if(note >= 10 && note <= 12) {
+    alert("Passable");
+    } else {
+        alert("bien");
+    }
