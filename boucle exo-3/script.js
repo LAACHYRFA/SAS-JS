2a4d42407e772ae55fd28a6cbec127f7180c2f25
LAACHYRFA <laachyr.farah.solicode@gmail.com>
2025-09-24 10:01:18 +0100

    Add files via upload

---
+++ boucle exo-3/script.js
@@ -0,0 +1,8 @@
+let x =prompt("enter number");
+let diviseurs="0";
+for(let i = 1; x<=i;i++);{
+   if (x % i===0){
+    diviseurs+=i+",";
+   }
+} 
+alert("Les diviseurs de X=" + X + " : " + diviseurs);
