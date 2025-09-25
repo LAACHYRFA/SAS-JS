59f9fa059fe4886ac8525bd9d98d897979ca251e
LAACHYRFA <laachyr.farah.solicode@gmail.com>
2025-09-25 09:25:25 +0100

    Add files via upload

---
+++ prototype/script.js
@@ -0,0 +1,59 @@
+while(true){
+let prototype = prompt ("entrer un numero entre C1..C8,T1..T3");
+if (prototype==="exit"){
+    alert(exit) 
+    break;
+
+}else if (prototype==="C1") {
+    alert("Maquetter une application");
+
+}else if (prototype==="C2"){
+    alert("Réaliser une interface utilisateur web statique et adaptable");
+
+}else if(prototype==="C3"){
+    alert("Développer une interface utilisateur web dynamique");
+
+}else if(prototype==="C4"){
+    alert("Réaliser une interface utilisateur avec une solution de gestion de contenu ou e-commerce");
+
+}else if(prototype==="C5"){
+    alert("Créer une base de données - niveau 1");
+
+}else if(prototype==="C6"){
+    alert("Développer les composants d’accès aux données - niveau 1");
+
+}else if(prototype==="C7"){
+    alert("Développer la partie back-end d’une application web ou web mobile - niveau 1");
+
+}else if(prototype==="C8"){
+alert("Framwork FrontEnd (React, vueJs...)");
+
+}else if(prototype==="T1"){
+    alert("Comprendre la démarche pédagogique à Solicode");
+
+}else if(prototype==="T2"){
+alert("Collaboration & posture professionnelle");
+
+}else if(prototype==="T3"){
+    alert("Gérer un projet avec Git & GitHub");
+
+}else{
+    alert("Cette compétence n'existe pas");
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
